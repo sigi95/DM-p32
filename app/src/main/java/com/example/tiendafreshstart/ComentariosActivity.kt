52c54ea8ec6b9bcbd996ec7373065611bcdc210a
juancampos3 <jucampos3@poligran.edu.co>
2021-12-03 23:16:31 -0500

    Reemplazo de Fragments a Activities

--- app/src/main/java/com/example/tiendafreshstart/ComentariosActivity.kt
+++ app/src/main/java/com/example/tiendafreshstart/ComentariosActivity.kt
@@ -2,14 +2,10 @@ package com.example.tiendafreshstart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.NumberPicker
 
-class MainActivity : AppCompatActivity() {
-
+class ComentariosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-   }
+        setContentView(R.layout.activity_comentarios)
+    }
 }
