e0e5da8e560263557d00031042ecf2ee4f88cc38
juancampos3 <jucampos3@poligran.edu.co>
2021-12-04 09:50:21 -0500

    Funcionalidad Cantidad de Productos en Detalle Producto y Carrito

--- app/src/main/java/com/example/tiendafreshstart/CarritoActivity.kt
+++ app/src/main/java/com/example/tiendafreshstart/CarritoActivity.kt
@@ -2,10 +2,32 @@ package com.example.tiendafreshstart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class CarritoActivity : AppCompatActivity() {
+
+    var numero:Int=1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_carrito)
+
+        val btnsumar=findViewById<Button>(R.id.btnSumarCarrito)
+        val btnrestar=findViewById<Button>(R.id.btnRestarCarrito)
+        val resultado=findViewById<TextView>(R.id.txtCantidadCarrito)
+
+        btnsumar.setOnClickListener {
+            numero = numero + 1
+            resultado.setText(String.format("%d",numero))
+        }
+
+        btnrestar.setOnClickListener {
+            if (numero >= 1) {
+                numero = numero - 1
+                resultado.setText(String.format("%d",numero))
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/tiendafreshstart/ProductDetailsActivity.kt
+++ app/src/main/java/com/example/tiendafreshstart/ProductDetailsActivity.kt
@@ -5,9 +5,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 
 class ProductDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        var numero:Int=1
+
+        val btnsumar2=findViewById<Button>(R.id.btnSumarProductDetails)
+        val btnrestar2=findViewById<Button>(R.id.btnRestarProductDetails)
+        val resultado2=findViewById<TextView>(R.id.txtCantidadProductDetails)
+
+        btnsumar2.setOnClickListener {
+            numero = numero + 1
+            resultado2.setText(String.format("%d",numero))
+        }
+
+        btnrestar2.setOnClickListener {
+            if (numero >= 1) {
+                numero = numero - 1
+                resultado2.setText(String.format("%d",numero))
+            }
+        }
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_details)
 
