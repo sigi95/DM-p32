bf6c769737906804e9a7a054b2dc41fd03176a31
juancampos3 <jucampos3@poligran.edu.co>
2021-12-04 00:09:26 -0500

    Comunicación entre Register a Login y Forms

--- app/src/main/java/com/example/tiendafreshstart/RegisterActivity.kt
+++ app/src/main/java/com/example/tiendafreshstart/RegisterActivity.kt
@@ -1,11 +1,29 @@
 package com.example.tiendafreshstart
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+
+        val btnIrForms2=findViewById<ImageView>(R.id.imgLogoRegister)
+
+        btnIrForms2.setOnClickListener {
+            val intentForms2 = Intent(this, FormsActivity::class.java)
+            startActivity(intentForms2)
+        }
+
+        val btnIrLogin2=findViewById<Button>(R.id.btnRegistroRegister)
+
+        btnIrLogin2.setOnClickListener {
+            val intentLogin2 = Intent(this, LoginActivity::class.java)
+            startActivity(intentLogin2)
+        }
     }
 }
