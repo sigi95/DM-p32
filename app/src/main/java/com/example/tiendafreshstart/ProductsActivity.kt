91416f8fa7bd8ca5ff69c04ae10974ba0a7be755
juancampos3 <jucampos3@poligran.edu.co>
2021-12-04 08:09:14 -0500

    Comunicación entre Products a ProductsDetails

--- app/src/main/java/com/example/tiendafreshstart/ProductsActivity.kt
+++ app/src/main/java/com/example/tiendafreshstart/ProductsActivity.kt
@@ -1,11 +1,35 @@
 package com.example.tiendafreshstart
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class ProductsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
+
+        val imgIrDetalleProducto1=findViewById<ImageView>(R.id.imageDetalle1)
+
+        imgIrDetalleProducto1.setOnClickListener {
+            val intentDetalleProducto1 = Intent(this, ProductDetailsActivity::class.java)
+            startActivity(intentDetalleProducto1)
+        }
+
+        val imgIrDetalleProducto2=findViewById<ImageView>(R.id.imageDetalle2)
+
+        imgIrDetalleProducto2.setOnClickListener {
+            val intentDetalleProducto2 = Intent(this, ProductDetailsActivity::class.java)
+            startActivity(intentDetalleProducto2)
+        }
+
+        val imgIrDetalleProducto3=findViewById<ImageView>(R.id.imageDetalle3)
+
+        imgIrDetalleProducto3.setOnClickListener {
+            val intentDetalleProducto3 = Intent(this, ProductDetailsActivity::class.java)
+            startActivity(intentDetalleProducto3)
+        }
+
     }
 }
