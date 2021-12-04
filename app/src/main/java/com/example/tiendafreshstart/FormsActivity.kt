f217f00786f994aa40a337a3cf444901f0483224
juancampos3 <jucampos3@poligran.edu.co>
2021-12-03 23:37:58 -0500

    Comunicación entre Forms y Register

--- app/src/main/java/com/example/tiendafreshstart/FormsActivity.kt
+++ app/src/main/java/com/example/tiendafreshstart/FormsActivity.kt
@@ -1,11 +1,22 @@
 package com.example.tiendafreshstart
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class FormsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forms)
+
+        val btnIrRegister=findViewById<Button>(R.id.btnRegistrarForms)
+
+        btnIrRegister.setOnClickListener {
+            val intentRegister = Intent(this, RegisterActivity::class.java)
+            startActivity(intentRegister)
+        }
+        
+        
     }
 }
